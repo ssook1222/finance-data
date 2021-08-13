6d3d0210f8225b487038858c4fa15eac56fc232a
ssook <60427387+ssook1222@users.noreply.github.com>
2021-08-14 03:42:23 +0900

    UPDATE : 2019, 2020 지역구별 업종 최소 매출 확인

--- choisook/profit_up.R
+++ choisook/profit_up.R
@@ -25,13 +25,130 @@ enterprise <- enterprise[ complete.cases(enterprise[ , c("X.영업이익총액")
 enterprise$X.영업이익총액 <- ifelse(enterprise$X.영업이익총액 < -5810949 | enterprise$X.영업이익총액 > 129031859, NA, enterprise$X.영업이익총액)
 enterprise <- enterprise[ complete.cases(enterprise[ , c("X.영업이익총액")]), ]
 
-locate<-enterprise %>% filter(X.시군구명 == "\"영등포구\""&X.기준년월=="\"2020-12\"") %>%
-  # 각 분류별로 묶기
-  select(X.영업이익총액,X.업종중분류명)
+enterprise$X.기준년월
 
-# 막대그래프로 표현
-ggplot(locate,aes(x=X.업종중분류명,y=X.영업이익총액))+
-  geom_bar(stat="identity")+
-  theme(text=element_text(size=4.5, family = "NanumGothic"))
+#각 지역구별 변수 생성
+중구<-enterprise %>% filter(X.시군구명 =="\"중구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
 
-ggsave("영등포.jpg", width=35,height=10,dpi = 600)
+종로구<-enterprise %>% filter(X.시군구명 =="\"종로구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+강남구<-enterprise %>% filter(X.시군구명 =="\"강남구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+강서구<-enterprise %>% filter(X.시군구명 =="\"강서구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+강동구<-enterprise %>% filter(X.시군구명 =="\"강동구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+강북구<-enterprise %>% filter(X.시군구명 =="\"강북구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+성북구<-enterprise %>% filter(X.시군구명 =="\"성북구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+노원구<-enterprise %>% filter(X.시군구명 =="\"노원구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+양천구<-enterprise %>% filter(X.시군구명 =="\"양천구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+마포구<-enterprise %>% filter(X.시군구명 =="\"마포구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+성동구<-enterprise %>% filter(X.시군구명 =="\"성동구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+광진구<-enterprise %>% filter(X.시군구명 =="\"광진구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+동작구<-enterprise %>% filter(X.시군구명 =="\"동작구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+도봉구<-enterprise %>% filter(X.시군구명 =="\"도봉구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+서대문구<-enterprise %>% filter(X.시군구명 =="\"서대문구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+송파구<-enterprise %>% filter(X.시군구명 =="\"송파구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+영등포구<-enterprise %>% filter(X.시군구명 =="\"영등포구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+은평구<-enterprise %>% filter(X.시군구명 =="\"은평구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+서초구<-enterprise %>% filter(X.시군구명 =="\"서초구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+중랑구<-enterprise %>% filter(X.시군구명 =="\"중랑구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+동대문구<-enterprise %>% filter(X.시군구명 =="\"동대문구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액))
+
+용산구<-enterprise %>% filter(X.시군구명 =="\"용산구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+관악구<-enterprise %>% filter(X.시군구명 =="\"관악구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+구로구<-enterprise %>% filter(X.시군구명 =="\"구로구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
+
+금천구<-enterprise %>% filter(X.시군구명 =="\"금천구\""&X.기준년월=="\"2019-12\"") %>%
+  select(X.영업이익총액,X.업종중분류명) %>%
+  group_by(X.업종중분류명) %>%
+  summarise(sum=sum(X.영업이익총액),min=min(X.영업이익총액))
