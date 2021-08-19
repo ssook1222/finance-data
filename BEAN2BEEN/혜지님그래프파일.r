b266fbd6cec540db2faf777ce79d22cc6f6700a2
BEAN2BEEN <87536808+BEAN2BEEN@users.noreply.github.com>
2021-08-19 11:57:47 +0900

    Create 혜지님그래프파일.r

---
+++ BEAN2BEEN/혜지님그래프파일.r
@@ -0,0 +1,23 @@
+#지역구별 이동인구와 가맹점매출입금 사이의 상관관계
+library(dplyr)
+please<-read.csv("C:\\Users\\Hanbeen Cho\\Desktop\\전체총합.csv")
+head(please)
+
+
+#결측치 포함 관측개체는 제거하기
+please<-please[complete.cases(please),]
+please
+table(is.na(please))
+
+#총수신금액+총소비금액 합하기
+please<- please %>% mutate(총계=총소비금액+총수신금액)
+head(please)
+attach(please)
+
+#회귀분석하기
+lm.please2<-lm(가맹점매출입금~지역구*이동인구, data=please)
+summary(lm.please2)
+
+#지역구가 범주형이므로 anova 실행
+anova(lm.please2)
+
