b9450c4682c60d1f0799ed3a54b201aa4c28135b
BEAN2BEEN <87536808+BEAN2BEEN@users.noreply.github.com>
2021-08-28 22:51:18 +0900

    Update 이동인구와 확진자.r

--- BEAN2BEEN/이동인구와 확진자.r
+++ BEAN2BEEN/이동인구와 확진자.r
@@ -16,6 +16,6 @@ cor.test(covidmov$이동인구, covidmov$확진자, method="pearson")
 
 #이동인구와 확진자 회귀분석
 library(ggplot2)
-ggplot(data = covidmov, aes(x= 이동인구, y= 확진자)) + geom_point() + stat_smooth(se=F, color = "#FC4E07", method = "lm")
+ggplot(data = covidmov, aes(x= 이동인구, y= 확진자)) + geom_point() +  coord_cartesian(xlim = c(0, 38000000), ylim = c(0, 600)) +stat_smooth(color = "#FC4E07", method = "lm")
 
 summary(lm(확진자~이동인구, data=covidmov))
